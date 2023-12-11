1442ccf68652d53c4d7168b89503c3c4c61799bf
德华 刘 <gavinprior5@gmail.com>
2023-12-11 16:55:33 +0000

    add text with icon

--- src/pages/auth/GetStarted.js
+++ src/pages/auth/GetStarted.js
@@ -21,6 +21,7 @@ const GetStarted = () => {
             d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
           />
         </svg>
+        <p className="text-3xl text-off-white">Let's Find New Jobs!</p>
       </div>
     </div>
   );
