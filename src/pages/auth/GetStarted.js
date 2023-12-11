b9d85d2fed5d7331b6fccfa7061a172e3d6918f3
德华 刘 <gavinprior5@gmail.com>
2023-12-11 23:02:44 +0000

    add blur configs for getting started

--- src/pages/auth/GetStarted.js
+++ src/pages/auth/GetStarted.js
@@ -3,25 +3,35 @@ import Blur from "../components/blur/Blur";
 import Button from "../components/buttons/buttons";
 
 const GetStarted = () => {
+  const blurConfigurations = [
+    {
+      className: "top-0 left-0 w-[80vw]",
+      fromColor: "dark",
+      toColor: "light-green",
+    },
+    {
+      className: "top-1/2 right-0 w-[50vw] rotate-180",
+      fromColor: "dark",
+      toColor: "dark-green",
+    },
+    {
+      className: "top-1/2 right-2/3 w-[20vw]",
+      fromColor: "dark",
+      toColor: "purple",
+    },
+  ];
   return (
     <div className="flex flex-col items-center justify-end h-screen w-full">
-      <Blur
-        className={`top-0 left-0 w-[80vw]`}
-        fromColor={`dark`}
-        toColor={`light-green`}
-      />
-      <Blur
-        className={`bottom-[260px] right-[-50px] w-[60vw] rotate-180`}
-        fromColor={`dark`}
-        toColor={`dark-green`}
-      />
-      <Blur
-        className={`top-1/2 right-2/3 w-[20vw] rotate-180`}
-        fromColor={`dark`}
-        toColor={`purple`}
-      />
+      {blurConfigurations.map(({ className, fromColor, toColor }, index) => (
+        <Blur
+          key={index}
+          className={className}
+          fromColor={fromColor}
+          toColor={toColor}
+        />
+      ))}
       {/* SVG jsx & text for job search */}
-      <div className="w-full h-[30vh] flex flex-col justify-evenly items-center mb-4">
+      <div className="w-full h-[30vh] flex flex-col justify-evenly items-center">
         <svg
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
@@ -39,7 +49,7 @@ const GetStarted = () => {
         <p className="text-3xl text-off-white">Let's Find New Jobs!</p>
       </div>
       {/* Button for job search */}
-      <Button className={`w-[95vw] bg-purple h-fit relative mb-4`}>
+      <Button className={`w-[95vw] bg-purple h-fit relative mb-6`}>
         <h3 className="text-xl text-dark upp">Get Started!</h3>
         <div className="w-[10%] aspect-square rounded-full bg-dark absolute right-[2.5vw] top-[10%] flex justify-center items-center">
           <svg
