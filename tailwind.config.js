b2ece128641da88dfdd2aa65efd593e55a22b0d5
德华 刘 <gavinprior5@gmail.com>
2023-12-11 16:06:03 +0000

    add color pallete to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,16 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    colors: {
+      dark: "#181a20",
+      charcoal: "#888888",
+      grey: "#b6b6b6",
+      "dark-green": "97e18d",
+      orange: "#f8b89a",
+      purple: "#ded3fc",
+      "light-green": "#e2fcde",
+      "off-white": "f8f8f8",
+    },
     extend: {},
   },
   plugins: [],
