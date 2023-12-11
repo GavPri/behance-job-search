4c75550126f85e2f14df692eb0750347f6e7a9f0
德华 刘 <gavinprior5@gmail.com>
2023-12-11 16:10:40 +0000

    positioning of blur wrapper

---
+++ src/pages/blur/Blur.js
@@ -0,0 +1,14 @@
+// The blur is a background effect that 
+// will be used on the get started page.
+
+import React from "react";
+
+const Blur = () => {
+  return (
+    <div className="absolute top-0 left-0 bg-dark">
+      <div></div>
+    </div>
+  );
+};
+
+export default Blur;
