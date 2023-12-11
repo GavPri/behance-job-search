f275356827dd0d27a96614a6eb3922a99e08abba
德华 刘 <gavinprior5@gmail.com>
2023-12-11 16:13:27 +0000

    add gradient & colors to blur

--- src/pages/blur/Blur.js
+++ src/pages/blur/Blur.js
@@ -6,7 +6,7 @@ import React from "react";
 const Blur = () => {
   return (
     <div className="absolute top-0 left-0 bg-dark">
-      <div className="w-[80vw] aspect-square"></div>
+      <div className="w-[80vw] aspect-square bg-gradient-to-r from-charcoal to-off-white"></div>
     </div>
   );
 };
