296840429485a4e8ecbc515b1f21e67f73353f65
德华 刘 <gavinprior5@gmail.com>
2023-12-11 16:44:34 +0000

    correct typo in config

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,7 @@ module.exports = {
       dark: "#181a20",
       charcoal: "#888888",
       grey: "#b6b6b6",
-      "dark-green": "97e18d",
+      "dark-green": "#97e18d",
       orange: "#f8b89a",
       purple: "#ded3fc",
       "light-green": "#e2fcde",
