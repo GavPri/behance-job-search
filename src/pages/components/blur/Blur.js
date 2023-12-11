7265b03069b447596612bd7853b3827aba0eb1f5
德华 刘 <gavinprior5@gmail.com>
2023-12-11 23:28:53 +0000

    comments on blur.js

--- src/pages/components/blur/Blur.js
+++ src/pages/components/blur/Blur.js
@@ -1,3 +1,8 @@
+// -----
+// The blur component is used in the background of the getting started page.
+// className is passed as prop to allow more styling of each individual blur. 
+// from color and toColor allow for manipulation of the gradient colors.
+// -----
 import React from "react";
 
 const Blur = ({ className, fromColor, toColor }) => {
