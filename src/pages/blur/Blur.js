c12ec2157bd75c3a24322cf325d4cf3bf90fcf59
德华 刘 <gavinprior5@gmail.com>
2023-12-11 20:49:49 +0000

    start cva for blur

--- src/pages/blur/Blur.js
+++ src/pages/blur/Blur.js
@@ -3,9 +3,10 @@ import React from "react";
 import { cva } from "class-variance-authority";
 
 const Blur = ({ position, color, size, gradientFrom, gradientTo, opacity }) => {
-  const containerStyle = `absolute ${position} top-0 left-0 bg-${color} blur-clip blur-lg`;
-  const overlayStyle = `w-[${size}vw] aspect-square bg-gradient-to-r from-${gradientFrom} to-${gradientTo} opacity-${opacity}`;
+  const blurVariants = cva(
 
+  )
+  
   return (
     <div className={containerStyle}>
       <div className={overlayStyle}></div>
