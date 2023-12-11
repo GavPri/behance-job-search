ec1cbbc44e2dbec95ec93fee9bf0af0fc22e7f5d
德华 刘 <gavinprior5@gmail.com>
2023-12-11 16:40:28 +0000

    add svg

--- src/pages/auth/GetStarted.js
+++ src/pages/auth/GetStarted.js
@@ -6,7 +6,22 @@ const GetStarted = () => {
     <div>
       <Blur />
       {/* SVG jsx for job search */}
-      <div className="w-full h-[30vh] bg-charcoal"></div>
+      <div className="w-full h-[30vh] bg-charcoal">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="w-6 h-6"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
+          />
+        </svg>
+      </div>
     </div>
   );
 };
