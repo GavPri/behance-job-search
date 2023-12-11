07999a944d6fcc45d774d0a24062e88b5a876f93
德华 刘 <gavinprior5@gmail.com>
2023-12-11 21:06:50 +0000

    create size and variants in cva

--- src/pages/blur/Blur.js
+++ src/pages/blur/Blur.js
@@ -4,7 +4,21 @@ import { cva } from "class-variance-authority";
 
 const Blur = ({ position, color, size, gradientFrom, gradientTo, opacity }) => {
   const blurVariants = cva(
-    "absolute bg-linear-to-r from-bg-dark to-light-green top-0 left-0 blur-clip"
+    "absolute bg-linear-to-r from-bg-dark to-light-green top-0 left-0 blur-clip w-fit aspect-square",
+    {
+      variants: {
+        variant: {
+          primary: "bg-linear-to-r from-bg-dark to-light-green top-0 left-0",
+          secondary: "bg-linear-to-r from-bg-grey to-dark-green top-4 right-0",
+          teritary: "bg-linear-to-r from-bg-charcoal to-purple top-6 left-2",
+        },
+        size: {
+          primary : 'w-[80vw]',
+          secondary: 'w-[30vw]',
+          teritary: 'w-[15vw]',
+        }
+      },
+    }
   );
 
   return (
