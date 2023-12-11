f6a381e30b9c759dcae91a12b4e75ef672345878
德华 刘 <gavinprior5@gmail.com>
2023-12-11 23:33:05 +0000

    add comments to buttons.js

--- src/pages/components/buttons/buttons.js
+++ src/pages/components/buttons/buttons.js
@@ -1,3 +1,13 @@
+/* -----
+  Button Component:
+
+  This React component represents a basic button with customizable styles.
+  
+  Props:
+  - children: Content to be rendered inside the button.
+  - onClick: Function to be called when the button is clicked. (to be added)
+  - className: Additional CSS classes to be applied to the button.
+----- */
 import React from "react";
 
 const Button = ({ children, onClick, className }) => {
