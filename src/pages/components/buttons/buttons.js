93159c1158b66de273c06f38e127f61e6feb6799
德华 刘 <gavinprior5@gmail.com>
2023-12-11 23:24:55 +0000

    increase y padding on button

--- src/pages/components/buttons/buttons.js
+++ src/pages/components/buttons/buttons.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Button = ({ children, onClick, className }) => {
   return (
-    <button className={`px-4 py-2 min-h-10 rounded-full ${className}`}>
+    <button className={`px-4 py-4 min-h-10 rounded-full ${className}`}>
       {children}
     </button>
   );
