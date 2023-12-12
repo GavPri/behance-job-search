adf32d46ef09ecae89c8c03db571ebc6792dfb6e
德华 刘 <gavinprior5@gmail.com>
2023-12-12 14:34:36 +0000

    remove props from blur

--- src/components/blur/Blur.js
+++ src/components/blur/Blur.js
@@ -1,18 +1,15 @@
 // -----
 // The blur component is used in the background of the getting started page.
-// className is passed as prop to allow more styling of each individual blur. 
+// className is passed as prop to allow more styling of each individual blur.
 // from color and toColor allow for manipulation of the gradient colors.
 // -----
 import React from "react";
 
-const Blur = ({ className, fromColor, toColor }) => {
+const Blur = () => {
   return (
-    <div className={`absolute ${className} bg-dark blur-clip blur-lg`}>
-      <div
-        className={`aspect-square bg-gradient-to-r from-${fromColor} to-${toColor} opacity-50`}
-      ></div>
+    <div className="absolute bg-dark blur-clip blur-lg">
+      <div className="aspect-square bg-gradient-to-r from-dark to-light-green opacity-50"></div>
     </div>
   );
 };
 export default Blur;
-
