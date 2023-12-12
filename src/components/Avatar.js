32636586c456443598c6b459cc667833286c6817
德华 刘 <gavinprior5@gmail.com>
2023-12-12 15:28:24 +0000

    try catch for request

--- src/components/Avatar.js
+++ src/components/Avatar.js
@@ -1,6 +1,15 @@
 import React from 'react'
 import axios from 'axios'
 
+const fetchProfileInfo = async () => {
+    try {
+        const { data } = axios("http://randomuser.me/api/?results=1");
+        console.log(data)
+    } catch (err) {
+        console.log(err)
+    }
+}
+
 const Avatar = () => {
   return (
     <div>Avatar</div>
