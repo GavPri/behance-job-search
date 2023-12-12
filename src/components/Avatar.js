debb69027bf80766c26b535808e66ecfd24382ef
德华 刘 <gavinprior5@gmail.com>
2023-12-12 16:24:06 +0000

    remove c logs from axios request

--- src/components/Avatar.js
+++ src/components/Avatar.js
@@ -4,9 +4,8 @@ import axios from "axios";
 const fetchProfileInfo = () => {
   axios.get("https://randomuser.me/api/").then((response) => {
     const user = response.data.results[0];
-    console.log(user);
     const userPicture = user.picture.medium;
-    console.log(userPicture);
+    
   });
 };
 fetchProfileInfo();
