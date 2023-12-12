bcc1a6ffd9cb90d29537af45b3681bdbb9f9912d
德华 刘 <gavinprior5@gmail.com>
2023-12-12 17:35:30 +0000

    create render svg function

--- src/pages/homepage/HomePage.js
+++ src/pages/homepage/HomePage.js
@@ -22,10 +22,35 @@ const HomePage = () => {
   }
   // Destructure properties
   const { name, picture } = userDetails;
+  // RenderSVG
+  const renderNotificationsSVG = () => {
+    return (
+      <div className="ml-auto">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="w-6 h-6 text-off-white mr-4"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0"
+          />
+        </svg>
+      </div>
+    );
+  };
   return (
-    <div>
+    <div className="flex items-center w-full">
       <Avatar userDetails={userDetails} />
-      <p>{name.first}</p>
+      <div className="flex flex-col justify-between items-start">
+        <p className="text-off-white">Good Morning</p>
+        <p className="font-bold text-off-white">{name.first}</p>
+      </div>
+      {renderNotificationsSVG()}
     </div>
   );
 };
