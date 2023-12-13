f0e013c426f70fd75fe011fe5e800d669dabecaf
德华 刘 <gavinprior5@gmail.com>
2023-12-13 15:20:56 +0000

    rendering categories in app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-
+import Categories from "./pages/homepage/Categories";
 import HomePageHeader from "./pages/homepage/HomePageHeader";
 
 function App() {
   return (
     <div className="bg-dark min-h-screen min-w-screen">
       <HomePageHeader />
+      <Categories />
     </div>
   );
 }
