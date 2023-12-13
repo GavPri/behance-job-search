0f41bcfacbb928c4fb19382e718a283be890fb9b
德华 刘 <gavinprior5@gmail.com>
2023-12-13 15:19:39 +0000

    boilerplat and style for heading

--- src/pages/homepage/Categories.js
+++ src/pages/homepage/Categories.js
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from "react";
 
 const Categories = () => {
   return (
-    <div>Categories</div>
-  )
-}
+    <div className="w-full h-fit px-4 bg-dark">
+      {/* Title  */}
+      <div className="w-full mb-2 flex">
+        <p className="text-off-white font-bold mr-auto ml-2">Categories</p>
+        <p className="text-dark-green ml-auto mr-2">See All</p>
+      </div>
+    </div>
+  );
+};
 
-export default Categories
+export default Categories;
